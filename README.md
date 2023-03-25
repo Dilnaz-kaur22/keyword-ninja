c35145afa25d926c148dc1342df3c0fb0e17ae61
Dilnaz-kaur22 <126363154+Dilnaz-kaur22@users.noreply.github.com>
2023-03-25 01:36:08 -0500

    Game Created

---
+++ README.md
@@ -0,0 +1,1 @@
+# keyword-ninja
---
+++ assets/script/index.js
@@ -0,0 +1,196 @@
+'use strict';
+
+const words = [
+  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+  'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component',
+  'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee',
+  'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet',
+  'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball',
+  'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management',
+  'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop',
+  'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing',
+  'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview',
+  'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory',
+  'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'
+];
+
+const timer = document.querySelector('.timer');
+const title = document.querySelector('.title');
+const play = document.querySelector('.play-button');
+const box = document.querySelector('.box');
+const word = document.querySelector('.word');
+const input = document.querySelector('.input');
+const score = document.querySelector('.score');
+const playAgain = document.querySelector('.play-again-button');
+
+const music = new Audio('./assets/audio/bg-music.mp3');
+music.loop = true;
+music.type = 'audio/mp3';
+
+const correctAnswer = new Audio('./assets/audio/correct.mp3');
+correctAnswer.type = 'audio/wav';
+
+const wrongAnswer = new Audio('./assets/audio/wrong.mp3');
+wrongAnswer.type = 'audio/mp3';
+
+class Score {
+  #date;
+  #hits;
+  #perc;
+  
+  constructor(date, hits, perc) {
+    this.#date = date;
+    this.hits = hits;
+    this.#perc = perc;
+  };
+  
+  set date(date) {this.#date = date;};
+  set hits(hits) {this.#hits = hits;};
+  set perc(perc) {this.#perc = perc;};
+  
+  get date() {return this.#date;};
+  get hits() {return this.#hits;};
+  get perc() {return this.#perc};
+  
+  getPercentage() {
+    this.#perc = (this.#hits * 100) / 90;
+  };
+  
+  getScore() {
+    return `${this.#date} | Hits: ${this.#hits} | ${this.#perc.toFixed(2)}%`;
+  };
+};
+
+let date = new Date().toDateString().slice(3, 10);
+let hits = 0;
+let perc;
+const player = new Score(date, hits, perc);
+
+function countdownTimer() {
+  let countdown = 90;
+  let countdownInterval = setInterval(() => {
+    countdown--;
+    timer.innerText = `Timer: ${countdown} Seconds`;
+    
+    if(countdown === 0) {
+      music.pause();
+      
+      clearInterval(countdownInterval);
+      
+      score.style.display = 'block'
+      playAgain.style.display = 'block';
+      
+      box.style.display = 'none';
+      word.style.display = 'none';
+      input.style.display = 'none';
+      
+      player.getPercentage();
+      score.innerText = player.getScore();
+    }
+  }, 1000);
+}
+
+function randomWord(words) {
+  let random = words[Math.floor(Math.random() * words.length)];
+  return random
+}
+
+play.addEventListener('click', () => {
+  music.play();
+  
+  title.style.display = 'none';
+  play.style.display = 'none';
+  
+  box.style.display = 'block';
+  word.style.display = 'block';
+  input.style.display = 'block';
+  
+  input.focus();
+  
+  countdownTimer();
+  
+  let random = randomWord(words);
+  
+  words.splice(words.indexOf(random), 1);
+  
+  word.innerText = random;
+  input.maxLength = random.length;
+});
+
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    if (input.value === word.innerText) {
+      correctAnswer.play();
+      
+      let random = randomWord(words);
+      
+      words.splice(words.indexOf(random), 1);
+      
+      input.classList.add('right');
+      
+      setTimeout(() => {
+        input.classList.remove('right');
+      }, 1000);
+      
+      word.innerText = random;
+      input.maxLength = random.length;
+      input.value = '';
+       
+      player.hits++;
+    } else {
+      wrongAnswer.play();
+      
+      input.classList.add('wrong');
+      
+      setTimeout(() => {
+        input.classList.remove('wrong');
+      }, 1000);
+    };
+  };
+});
+
+playAgain.addEventListener('click', () => {
+  music.play();
+  score.style.display = 'none';
+  playAgain.style.display = 'none';
+  
+  box.style.display = 'block';
+  word.style.display = 'block';
+  input.style.display = 'block';
+  
+  const words = [
+    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+    'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+    'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+    'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+    'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component',
+    'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee',
+    'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet',
+    'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball',
+    'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management',
+    'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop',
+    'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing',
+    'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview',
+    'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory',
+    'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'
+  ];
+  
+  player.date = new Date().toDateString().slice(3, 10);
+  player.hits = 0;
+  player.perc;
+  
+  score.innerText = '';
+  
+  input.focus();
+  
+  countdownTimer();
+  
+  let random = randomWord(words);
+  
+  word.innerText = random;
+  input.maxLength = random.length;
+  input.value = '';
+});
